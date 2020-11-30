0f9fb860ae9545947bc985846022665184423439
권민정 <kmjdec1203@gmail.com>
2020-11-30 22:52:23 +0900

    [Add] 자바 덧셈

---
+++ 코드/0624-Study/First.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a;
+        int b;
+
+        System.out.println(scanner.nextInt() + scanner.nextInt());
+    }
+}
