40adb6649d2b1afd52b68b281a07b938d78abae0
권민정 <kmjdec1203@gmail.com>
2021-02-06 03:29:17 +0900

    [Add] Stack
    Stack 코드

---
+++ C/자료구조/Stack/Stack.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MX 5
+
+typedef struct _stack {
+	int Arr[MX];
+	int sp;
+} Stack;
+
+void StackInit(Stack* S) {
+	S->sp = -1;
+}
+
+int isEmpty(Stack* S) {
+	if (S->sp < 0)
+		return 1;
+	else
+		return 0;
+}
+
+int isFull(Stack* S) {
+	if (S->sp + 1 >= MX)
+		return 1;
+	else
+		return 0;
+}
+
+void Push(Stack* S, int x) {
+	if (isFull(S)) {
+		printf("Stack is Full!\n");
+		exit(0);
+	}
+	else
+		S->Arr[++(S->sp)] = x;
+}
+
+int Pop(Stack* S) {
+	int tmp;
+	if (isEmpty(S)) {
+		printf("  Stack is Empty!\n");
+		exit(0);
+	}
+	else {
+		tmp = S->Arr[S->sp];
+		S->sp--;
+		return tmp;
+	}
+}
+
+int main() {
+	Stack* S = (Stack*)malloc(sizeof(Stack));
+	int tmp;
+
+	StackInit(S);
+
+	printf("1"); Push(S, 6);
+	printf("2"); Push(S, 5);
+	printf("3"); Push(S, 4);
+	printf("4"); Push(S, 3);
+	printf("5"); Push(S, 2);
+	//printf("6"); Push(S, 1);
+
+	tmp = Pop(S); printf("%3d", tmp);
+	tmp = Pop(S); printf("%3d", tmp);
+	tmp = Pop(S); printf("%3d", tmp);
+	tmp = Pop(S); printf("%3d", tmp);
+}
